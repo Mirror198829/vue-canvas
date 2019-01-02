398f9ef9e8d9d63d71aedd1ab2a0f78c0ecb2722
Mirror198829 <412366756@qq.com>
2019-01-02 14:03:07 +0800

    add mouseDraw

---
+++ src/pages/mouseDraw.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: caojing
+ * @Date: 2019-01-02 14:02:18
+ * @LastEditors: caojing
+ * @LastEditTime: 2019-01-02 14:02:34
+ * @Description: draw by mouse
+ -->
+<template>
+
+</template>
+
+<script>
+export default {
+  name:'',
+  data(){
+   return {
+
+   }
+  },
+  components: {
+
+  },
+  methods:{
+
+  },
+  mounted(){
+
+  },
+  created(){}
+}
+</script>
+
+<style lang="less">
+
+</style>
+<style lang="less" scoped>
+
+</style>
