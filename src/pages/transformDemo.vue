42bc0409f6e0a69f82497be2624681458e2ef6d2
Mirror198829 <412366756@qq.com>
2019-01-08 11:50:59 +0800

    增加三个小块

--- src/pages/transformDemo.vue
+++ src/pages/transformDemo.vue
@@ -2,39 +2,70 @@
  * @Author: caojing
  * @Date: 2019-01-08 11:25:47
  * @LastEditors: caojing
- * @LastEditTime: 2019-01-08 11:26:10
+ * @LastEditTime: 2019-01-08 11:50:08
  * @Description: canvas变换示例
  -->
 <template>
-<div>
-    123
-</div>
+  <div class="tranformDemo">
+    <div class="canvasWrap">
+      <div class="canvasItem">
+        <canvas width="300" height="300"></canvas>
+      </div>
+      <div class="canvasItem">
+        <canvas width="300" height="300"></canvas>
+      </div>
+      <div class="canvasItem">
+        <canvas width="300" height="300"></canvas>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-export default {
-  name:'',
-  data(){
-   return {
+  export default {
+    name: '',
+    data() {
+      return {
 
-   }
-  },
-  components: {
+      }
+    },
+    components: {
 
-  },
-  methods:{
+    },
+    methods: {
 
-  },
-  mounted(){
+    },
+    mounted() {
 
-  },
-  created(){}
-}
+    },
+    created() {}
+  }
+
 </script>
 
 <style lang="less">
 
 </style>
 <style lang="less" scoped>
+  .tranformDemo {
+    height: 100%;
+    width: 100%;
+    background: #333;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+  }
+
+  .canvasWrap {
+    width: 950px;
+    display: flex;
+    align-items: start;
+    justify-content:space-between;
+  }
+
+  .canvasItem {
+    background: #fff;
+  }
 
 </style>
