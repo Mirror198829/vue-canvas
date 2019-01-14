0f46a82ff49b856be73ed20ca760ad17556ecba7
Mirror198829 <412366756@qq.com>
2019-01-14 16:05:27 +0800

    修改

--- src/pages/imgDemo.vue
+++ src/pages/imgDemo.vue
@@ -2,11 +2,13 @@
  * @Author: caojing
  * @Date: 2019-01-11 16:22:52
  * @LastEditors: caojing
- * @LastEditTime: 2019-01-11 17:02:19
+ * @LastEditTime: 2019-01-14 15:59:48
  * @Description:图片处理相关demo 
  -->
 <template>
 <div class="imgDemo">
+    <el-button> - </el-button>
+    <input type="button" value="->">
     <canvas width="400" height="400" id="c1"></canvas>
 </div>
 </template>
@@ -28,7 +30,7 @@ export default {
       let ctx = c.getContext('2d')
       let yImg = new Image()
       yImg.onload = ()=>{
-        ctx.drawImage(yImg,30,30)
+        ctx.drawImage(yImg,30,30,200,40)
       }
       yImg.src = 'src/assets/logo.png'  
     }
