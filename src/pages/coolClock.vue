ee88d3d7dec2374dcd72bac2db09bfc4345e7717
Mirror198829 <412366756@qq.com>
2019-01-16 16:51:50 +0800

    优化格式

--- src/pages/coolClock.vue
+++ src/pages/coolClock.vue
@@ -28,22 +28,22 @@
         let [hourMaxL, hourMinL] = [this.L, this.L - 28] //获取时刻度首位距离x0,y0位置
         let [minMaxL, minMinL] = [this.L - 2, this.L - 15]
         let centerL = 15
-        let oDate = new Date()//绑定当前时间
+        let oDate = new Date() //绑定当前时间
         let oHours = oDate.getHours()
         let oMin = oDate.getMinutes()
         let oSen = oDate.getSeconds()
-        let oHoursValue = -90 + oHours*30 + oMin/2 //获取小时的角度
-        let oMinValue = -90 + oMin*6
-        let oSenValue = -90 + oSen*6
-        ctx.clearRect(0,0,canvas.width,canvas.height) //清除画布
+        let oHoursValue = -90 + oHours * 30 + oMin / 2 //获取小时的角度
+        let oMinValue = -90 + oMin * 6
+        let oSenValue = -90 + oSen * 6
+        ctx.clearRect(0, 0, canvas.width, canvas.height) //清除画布
         this.drawHoursScale(ctx, x0, y0, 12, 9, hourMaxL, hourMinL) //绘制表盘的时钟刻度scale
         this.drawHoursScale(ctx, x0, y0, 60, 3, minMaxL, minMinL) //绘制表盘分钟刻度
         this.drawScaleNum(ctx, hourMinL - 25, x0, y0) //绘制12个表盘数字
-        this.drawTimeNeedle(ctx,x0,y0,15,135,oHoursValue)//绘制时针 
-        this.drawTimeNeedle(ctx,x0,y0,8,minMaxL - 15,oMinValue) //绘制分针  
-        this.drawTimeNeedle(ctx,x0,y0,3,minMaxL - 2,oSenValue,'#f3a829') //绘制秒针
-        this.drawSenDotted(ctx,'#f3a829',x0,y0,oSenValue-180,centerL)//绘制秒针尾部
-        this.drawCircleCenter(ctx,x0,y0,'#f3a829',centerL)//绘制圆心
+        this.drawTimeNeedle(ctx, x0, y0, 15, 135, oHoursValue) //绘制时针 
+        this.drawTimeNeedle(ctx, x0, y0, 8, minMaxL - 15, oMinValue) //绘制分针  
+        this.drawTimeNeedle(ctx, x0, y0, 3, minMaxL - 2, oSenValue, '#f3a829') //绘制秒针
+        this.drawSenDotted(ctx, '#f3a829', x0, y0, oSenValue - 180, centerL) //绘制秒针尾部
+        this.drawCircleCenter(ctx, x0, y0, '#f3a829', centerL) //绘制圆心
       },
       drawHoursScale(ctx, x0, y0, scaleNum, scaleW, maxL, minL) {
         for (let i = 0; i < scaleNum; i++) {
@@ -76,45 +76,45 @@
           ctx.restore()
         }
       },
-      drawTimeNeedle(ctx,x0,y0,lineW,L,angel,color='#000'){
-         let [x,y] = [x0 + Math.cos(angel * Math.PI / 180) * L, y0 + Math.sin(angel * Math.PI / 180) * L] 
-         ctx.save()
-         ctx.beginPath()
-         ctx.strokeStyle = color
-         ctx.lineWidth = lineW
-         ctx.lineCap = "round"
-         ctx.moveTo(x0,y0)
-         ctx.lineTo(x,y)
-         ctx.stroke()
-         ctx.closePath()
-         ctx.restore()
+      drawTimeNeedle(ctx, x0, y0, lineW, L, angel, color = '#000') {
+        let [x, y] = [x0 + Math.cos(angel * Math.PI / 180) * L, y0 + Math.sin(angel * Math.PI / 180) * L]
+        ctx.save()
+        ctx.beginPath()
+        ctx.strokeStyle = color
+        ctx.lineWidth = lineW
+        ctx.lineCap = "round"
+        ctx.moveTo(x0, y0)
+        ctx.lineTo(x, y)
+        ctx.stroke()
+        ctx.closePath()
+        ctx.restore()
       },
-      drawCircleCenter(ctx,x0,y0,color,L){
-          ctx.save()
-          ctx.beginPath()
-          ctx.shadowBlur = 4
-          ctx.shadowColor = "#000"
-          ctx.fillStyle = color
-          ctx.arc(x0,y0,L,0,360*Math.PI/180)
-          ctx.fill()
-          ctx.closePath()
-          ctx.restore()
+      drawCircleCenter(ctx, x0, y0, color, L) {
+        ctx.save()
+        ctx.beginPath()
+        ctx.shadowBlur = 4
+        ctx.shadowColor = "#000"
+        ctx.fillStyle = color
+        ctx.arc(x0, y0, L, 0, 360 * Math.PI / 180)
+        ctx.fill()
+        ctx.closePath()
+        ctx.restore()
       },
-      drawSenDotted(ctx,color,x0,y0,angel,L){
-        let [x,y] = [x0 + Math.cos(angel * Math.PI / 180) * L, y0 + Math.sin(angel * Math.PI / 180) * L]  
+      drawSenDotted(ctx, color, x0, y0, angel, L) {
+        let [x, y] = [x0 + Math.cos(angel * Math.PI / 180) * L, y0 + Math.sin(angel * Math.PI / 180) * L]
         ctx.save()
         ctx.beginPath()
         ctx.fillStyle = color
-        ctx.arc(x,y,7,0,360*Math.PI/180)
+        ctx.arc(x, y, 7, 0, 360 * Math.PI / 180)
         ctx.fill()
         ctx.closePath()
         ctx.restore()
       }
     },
     mounted() {
-      setInterval(()=>{
+      setInterval(() => {
         this.drawCoolClock()
-      },1000)
+      }, 1000)
     },
     created() {}
   }
@@ -129,12 +129,11 @@
     width: 100%;
     height: 100%;
     background: radial-gradient(#f8f6f5, #f7f5f4, #e4e3e2, #b9b8b7);
-    display:flex;
-    align-items:center;
+    display: flex;
+    align-items: center;
     justify-content: center;
   }
 
-  #coolClockCanvas {
-  }
+  #coolClockCanvas {}
 
 </style>
