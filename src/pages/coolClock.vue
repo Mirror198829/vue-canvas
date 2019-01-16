953f11556ec89d49798d243cfc80284d28ba2c8f
Mirror198829 <412366756@qq.com>
2019-01-16 14:45:13 +0800

    增加一个炫酷闹钟页面

---
+++ src/pages/coolClock.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: caojing
+ * @Date: 2019-01-16 14:40:40
+ * @LastEditors: caojing
+ * @LastEditTime: 2019-01-16 14:41:08
+ * @Description: 炫酷时钟 canvas+vue
+ -->
+<template>
+<div>
+    123
+</div>
+</template>
+
+<script>
+export default {
+  name:'',
+  data(){
+   return {
+
+   }
+  },
+  components: {
+
+  },
+  methods:{
+
+  },
+  mounted(){
+
+  },
+  created(){}
+}
+</script>
+
+<style lang="less">
+
+</style>
+<style lang="less" scoped>
+
+</style>
