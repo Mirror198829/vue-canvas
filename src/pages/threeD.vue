e56e5da7c59083348b87ec62aebb50de585645b8
Mirror198829 <412366756@qq.com>
2019-03-26 17:05:32 +0800

    完成布局样式

--- src/pages/threeD.vue
+++ src/pages/threeD.vue
@@ -2,18 +2,18 @@
  * @Author: caojing
  * @Date: 2019-03-26 14:29:42
  * @LastEditors: caojing
- * @LastEditTime: 2019-03-26 16:30:45
+ * @LastEditTime: 2019-03-26 17:04:19
  * @Description: 3d动画交互
  -->
 <template>
-  <div>
+  <div style="position:relative">
     <div class="sence">
       <div class="box">
         <ul class="ring"></ul>
       </div>
       <div class="item">
         <p>阿斯顿发顺丰大撒旦发射点发射点发射点发阿斯顿发顺丰大撒旦发射点发射点发射点发阿斯顿发顺丰大撒旦发射点发射点发射点发阿斯顿发顺丰大撒旦发射点发射点发射点发</p>
-        <i class="fa fa-close itemClose"></i>
+        <i class="fa fa-close close"></i>
       </div>
     </div>
     <ul class="table">
@@ -27,6 +27,15 @@
       <button>展开</button>
       <button>自定义文字</button>
     </div>
+    <div class="text" v-show="textShow">
+      <p>在下面输入你自己的文字</p>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <div class="btn2">
+        <button>提交</button>
+        <button>重置</button>
+        <i class="fa fa-close close" @click="textShow=false"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,7 +44,7 @@
     name: '',
     data() {
       return {
-
+        textShow:true,
       }
     },
     components: {
@@ -57,7 +66,37 @@
 </style>
 <style lang="less" scoped>
   @base-color: #889199;
-  @theme-color:#00a0e9;
+  @theme-color: #00a0e9;
+
+  .btnClass {
+    border: 1px solid #cacaca;
+    outline: none;
+    transition: all .2s;
+    background: linear-gradient(#fff, #d6e1eb);
+    cursor: pointer;
+    color: @base-color;
+
+    &:hover {
+      color: @theme-color;
+      background: linear-gradient(#bef5ff, #77c5eb);
+      border-color: @theme-color;
+    }
+
+    &:active {
+      color: @theme-color;
+      background: linear-gradient(#77c5eb, #bef5ff);
+      border-color: @theme-color;
+    }
+  }
+  .close{
+    color:@theme-color;
+    position:absolute;
+    top:10px;
+    right:10px;
+    font-size:18px;
+    cursor: pointer;
+  }
+
   .sence {
     width: 400px;
     height: 400px;
@@ -96,7 +135,7 @@
       p {
         font: 14px/24px '微软雅黑';
         word-break: break-all;
-        color:@base-color
+        color: @base-color
       }
 
       .itemClose {
@@ -116,29 +155,93 @@
     justify-content: space-between;
 
     li {
-      a{color: @base-color;text-decoration:none;
-      &:hover{
-        color:@theme-color;
-      }}
+      a {
+        color: @base-color;
+        text-decoration: none;
+
+        &:hover {
+          color: @theme-color;
+        }
+      }
     }
   }
-  .line{
-    width:734px;height:1px;margin:0 auto 14px;background:linear-gradient(to right,@theme-color,#8cca94,#fff45c)
+
+  .line {
+    width: 734px;
+    height: 1px;
+    margin: 0 auto 14px;
+    background: linear-gradient(to right, @theme-color, #8cca94, #fff45c)
   }
-  .btn{
-    width:204px;height:30px;margin:0 auto;display:flex;justify-content:center;
-    button{
-      width:100px;height:100%;border:1px solid #cacaca;background:linear-gradient(#fff,#d6e1eb);cursor: pointer;color:@base-color;
-      &:hover{
-        color:@theme-color;
-        background:linear-gradient(#bef5ff,#77c5eb);
-        border-color:@theme-color;
+
+  .btn {
+    width: 204px;
+    height: 30px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+
+    button {
+
+      width: 100px;
+      height: 100%;
+      .btnClass;
+      &:nth-of-type(1) {
+        border-radius: 15px 0 0 15px;
       }
-      &:nth-of-type(1){
-        border-radius:15px 0 0 15px;
+
+      &:nth-of-type(2) {
+        border-radius: 0 15px 15px 0;
+        margin-left: -1px;
       }
-      &:nth-of-type(2){
-        border-radius:0 15px 15px 0; margin-left:-1px;
+    }
+  }
+
+  .text {
+    width: 602px;
+    height: 318px;
+    padding: 15px 18px;
+    border: 1px solid @base-color;
+    position: absolute;
+    left: 50%;
+    margin-left: -329px;
+    top: 30px;
+    background: #fff;
+    border-radius: 8px;
+
+    p {
+      font-size: 12px;
+      margin-bottom: 15px;
+      text-align: left;
+    }
+
+    textarea {
+      resize: none;
+      width: 580px;
+      height: 220px;
+      padding: 10px;
+      border: 1px solid @base-color;
+      border-radius: 8px;
+      background: #f3f7fa;
+      margin-bottom: 15px;
+      outline: none;
+    }
+
+    .btn2 {
+      width: 260px;
+      height: 30px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+
+      button {
+        cursor: pointer;
+        width: 120px;
+        height: 100%;
+        border-radius: 15px;
+        .btnClass;
+        &:nth-of-type(1){
+          margin-right:15px;
+        }
       }
     }
   }
